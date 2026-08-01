45ec63bbf1b34900adcb2ffef9168035b526c236
V.J. Manzo <3967529+vjmanzo@users.noreply.github.com>
2026-08-01 03:44:37 +0000

    Prepare release 0.2.3

--- NeuralAmpModeler/config.h
+++ NeuralAmpModeler/config.h
@@ -1,7 +1,7 @@
 #define PLUG_NAME "Puke Amp"
 #define PLUG_MFR "Puke"
-#define PLUG_VERSION_HEX 0x00000202
-#define PLUG_VERSION_STR "0.2.2"
+#define PLUG_VERSION_HEX 0x00000203
+#define PLUG_VERSION_STR "0.2.3"
 #define STATE_VERSION_STR "0.7.16"
 #define PLUG_UNIQUE_ID 'PkAm'
 #define PLUG_MFR_ID 'SDAa'
